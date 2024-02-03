1ce59843c8b3ad26b8c68ae529e63acf371e31ff
Ancel Dev account <anceldev@Ancels-MacBook-Pro.local>
2024-02-03 14:47:41 +0100

    Firebase emulator settings

--- Chrat/ChratApp.swift
+++ Chrat/ChratApp.swift
@@ -10,14 +10,24 @@ import FirebaseCore
 import FirebaseAuth
 import FirebaseFirestore
 import FirebaseFirestoreSwift
+import FirebaseStorage
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
       
       FirebaseApp.configure()
-      
+      /// Firebase Emulator settings
+      /// Authentication
       Auth.auth().useEmulator(withHost: "localhost", port: 9099)
+      /// Firestore
+      let settings = Firestore.firestore().settings
+      settings.host = "127.0.0.1:8080"
+      settings.isSSLEnabled = false
+      Firestore.firestore().settings = settings
+      /// Cloud Storage
+      Storage.storage().useEmulator(withHost: "127.0.0.1", port: 9199)
+      
             
     return true
   }
